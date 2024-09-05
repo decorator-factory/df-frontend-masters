3f39f46921650dd514c818e1d2fe1fda57c7bb06
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2024-09-05 06:19:10 +0300

    I somehow managed to mistype the name of the website

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Some of the content on this repo is taken from https://www.frontendmentor.io.
 
-Go to https://decorator-factory.github.io/df-frontend-masters/ for an index of challenges.
+Go to https://decorator-factory.github.io/df-frontend-mentor/ for an index of challenges.
 
 To run locally, install Podman and run `local.sh`.
--- local.sh
+++ local.sh
@@ -2,4 +2,4 @@
 
 echo 'Open http://127.0.0.1:58008/ in your browser'
 echo ''
-podman run --rm --name frontend-masters -p 127.0.0.1:58008:80 -v ./challenges:/usr/local/apache2/htdocs/:ro docker.io/httpd:2.4
+podman run --rm --name frontend-mentor -p 127.0.0.1:58008:80 -v ./challenges:/usr/local/apache2/htdocs/:ro docker.io/httpd:2.4
